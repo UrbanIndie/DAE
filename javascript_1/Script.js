4b8b3c05ef4748ca6beb40bc95ca785ec7697b94
UrbanIndie <ishajaishiva@gmail.com>
2026-03-18 18:17:30 -0400

    js1 done

--- javascript_1/Script.js
+++ javascript_1/Script.js
@@ -8,6 +8,10 @@ let extra = parseInt (prompt("Please Enter The Number Of Assumed Steps: "));
 
 you.innerHTML = "Hello, " + name;
 
+let x = document.getElementById("js");
+x.textContent = "Welcome To The Channa Masala Website";
+console.log(x);
+
 const me = name
 
 steps = steps + extra
@@ -26,3 +30,14 @@ else {
 if (steps == 8 && name == me) {
   console.log ("8 Steps With No Extra Steps By The Name, " + me);
 }
+
+function step (extra) {
+    let counter = 0;
+    while (counter < extra) {
+    console.log ("Counter is at: " + counter);
+    console.log ("Counter is at: " + extra)
+    counter++
+    }
+}
+
+step (extra);
